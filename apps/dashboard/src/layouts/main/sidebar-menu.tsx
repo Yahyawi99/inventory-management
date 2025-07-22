8afce055a913c67d6a4a1b184aa13f5c8ef12b30
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-07-22 20:37:58 +0100

    feat: working on side bar

--- apps/dashboard/src/layouts/main/sidebar-menu.tsx
+++ apps/dashboard/src/layouts/main/sidebar-menu.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import {
   SidebarMenu,
   SidebarMenuButton,
@@ -13,10 +12,11 @@ import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "@radix-ui/react-collapsible";
+} from "@/components/ui/collapsible";
 import { MenuItemType } from "@/types/sidebar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function MySidebarMenu({ item }: { item: MenuItemType }) {
   const { id, name, icon, href, subMenuItems } = item; // destructure icon
@@ -24,12 +24,22 @@ export default function MySidebarMenu({ item }: { item: MenuItemType }) {
   return (
     <SidebarMenu>
       {subMenuItems.length ? (
-        <Collapsible className="mb-3 text-(--border)">
+        <Collapsible className="group mb-3 text-(--ring)" defaultOpen={false}>
           <SidebarMenuItem key={id}>
-            <CollapsibleTrigger asChild>
-              <SidebarMenuButton>
-                <FontAwesomeIcon icon={icon} />
-                <span className="text-[1.25rem]">{name}</span>
+            <CollapsibleTrigger
+              className="data-[state=open]:hover:text-(--border) hover:text-(--border) data-[state=open]:active:text-(--border) data-[state=closed]:active:text-(--border)"
+              asChild
+            >
+              <SidebarMenuButton className="flex justify-between hover:!bg-transparent focus:!bg-transparent active:!bg-transparent hover:text-(--border)">
+                <div>
+                  <FontAwesomeIcon icon={icon} className="text-inherit mr-2" />
+                  <span className="text-[1.25rem] font-bold ">{name}</span>
+                </div>
+
+                <FontAwesomeIcon
+                  icon={faChevronRight}
+                  className="transition-transform group-data-[state=open]:rotate-90 text-inherit"
+                />
               </SidebarMenuButton>
             </CollapsibleTrigger>
 
@@ -39,8 +49,11 @@ export default function MySidebarMenu({ item }: { item: MenuItemType }) {
                   const { id: subId, name: subItemName, href } = subItem;
                   return (
                     <SidebarMenuSubItem key={subId}>
-                      <SidebarMenuSubButton asChild>
-                        <Link href={href}>
+                      <SidebarMenuSubButton
+                        className="hover:!bg-transparent focus:!bg-transparent active:!bg-transparent  text-(--ring) decoration-3 hover:underline hover:text-(--ring) "
+                        asChild
+                      >
+                        <Link href={href!}>
                           <span className="text-[1rem]">{subItemName}</span>
                         </Link>
                       </SidebarMenuSubButton>
@@ -52,10 +65,13 @@ export default function MySidebarMenu({ item }: { item: MenuItemType }) {
           </SidebarMenuItem>
         </Collapsible>
       ) : (
-        <SidebarMenuItem key={id} className="mb-3 px-2 text-(--border)">
+        <SidebarMenuItem
+          key={id}
+          className="mb-3 px-2 text-(--ring) hover:text-(--border)"
+        >
           <FontAwesomeIcon icon={icon} className="mr-2" />
           <Link href={href as string}>
-            <span className="text-[1.25rem]">{name}</span>
+            <span className="text-[1.25rem] font-bold">{name}</span>
           </Link>
         </SidebarMenuItem>
       )}
