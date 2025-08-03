a134b662932802b60e0e644f6dcd8c3636d74f16
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-08-03 19:05:28 +0100

    feat: axios

---
+++ packages/utils/axios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    console.error("API Error:", err);
+    return Promise.reject(err);
+  }
+);
+
+export default api;
