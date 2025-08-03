f4f391ea1882af3242a79bed8b91f75337da63c9
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-08-03 18:54:35 +0100

    feat: worked on order repository and create basic GET routes

---
+++ packages/services/repositories/order.repository.ts
@@ -0,0 +1,42 @@
+import Prisma from "database";
+import {
+  OrderStatus,
+  OrderType,
+  Prisma as P,
+} from "database/generated/prisma/client";
+
+type Filters = {
+  orderDate?: Date;
+  status?: OrderStatus;
+  orderType?: OrderType;
+};
+
+export const OrderRepository = {
+  async findMany(companyId: string, filters?: Filters, search?: string) {
+    try {
+      const res = await Prisma.order.findMany({
+        where: {
+          companyId,
+        },
+      });
+
+      return res;
+    } catch (e) {
+      console.log("Error while fetching orders");
+      console.log(e);
+    }
+  },
+
+  async findById(id: string) {
+    try {
+      const res = await Prisma.order.findUnique({
+        where: {
+          id,
+        },
+      });
+    } catch (e) {
+      console.log("Error while fetching order " + id);
+      console.log(e);
+    }
+  },
+};
