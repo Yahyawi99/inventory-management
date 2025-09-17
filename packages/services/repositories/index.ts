f6681f9811dcc664d014d8a03209f6724ba95de6
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-09-17 21:44:17 +0100

    refactor: created invoice repository

--- packages/services/repositories/index.ts
+++ packages/services/repositories/index.ts
@@ -2,3 +2,4 @@ export * from "./order.repository";
 export * from "./product.repository";
 export * from "./category.repository";
 export * from "./user.repository";
+export * from "./invoice.repository";
---
+++ packages/services/repositories/invoice.repository.ts
@@ -0,0 +1,103 @@
+import Prisma from "database";
+import {
+  InvoiceStatus,
+  OrderStatus,
+  OrderType,
+  Invoice,
+  Prisma as P,
+} from "database/generated/prisma/client";
+
+interface Filters {
+  status?: InvoiceStatus[];
+  search?: string;
+  orderType?: OrderType;
+}
+
+interface SortConfig {
+  field: string;
+  direction: "desc" | "asc";
+}
+
+export const InvoiceRepository = {
+  async findMany(
+    orgId: string,
+    userId: string,
+    filters: Filters,
+    orderBy: SortConfig = { field: "invoiceDate", direction: "desc" },
+    { page, pageSize }: { page: number; pageSize: number }
+  ): Promise<{ totalPages: number; invoices: Invoice[] } | null> {
+    // Where clause
+    const whereClause: P.InvoiceWhereInput = {
+      organizationId: orgId,
+      userId,
+    };
+
+    if (filters.search) {
+      whereClause.OR = [
+        { invoiceNumber: { contains: filters.search, mode: "insensitive" } },
+        {
+          order: {
+            customer: {
+              name: { contains: filters.search, mode: "insensitive" },
+            },
+            supplier: {
+              name: { contains: filters.search, mode: "insensitive" },
+            },
+          },
+        },
+      ];
+    }
+
+    if (filters.orderType) {
+      whereClause.order = {
+        orderType: filters.orderType,
+      };
+    }
+
+    if (filters.status) {
+      whereClause.status = { in: filters.status };
+    }
+
+    // OrderBy clause
+    const orderByClause: P.InvoiceOrderByWithRelationInput = {};
+    if (orderBy) {
+      orderByClause[orderBy.field as keyof P.InvoiceOrderByWithRelationInput] =
+        orderBy.direction;
+    }
+
+    try {
+      const res = await Prisma.invoice.findMany({
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+        where: whereClause,
+        orderBy: orderByClause,
+        include: {
+          order: true,
+        },
+      });
+
+      const totalOrders = await Prisma.invoice.count({
+        where: whereClause,
+      });
+      const totalPages = Math.ceil(totalOrders / pageSize);
+
+      return { totalPages, invoices: res };
+    } catch (e) {
+      console.log("Error while fetching orders: ", e);
+      return null;
+    }
+  },
+
+  async findById(id: string) {
+    try {
+      const res = await Prisma.order.findUnique({
+        where: {
+          id,
+        },
+      });
+    } catch (e) {
+      console.log("Error while fetching order " + id);
+      console.log(e);
+    }
+  },
+};
