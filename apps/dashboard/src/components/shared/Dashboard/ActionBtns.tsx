70b7d5782b21d9240dc9420834ec0c6272f668e6
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-01-19 22:38:42 +0100

    i18n : dashboard page

--- apps/dashboard/src/components/shared/Dashboard/ActionBtns.tsx
+++ apps/dashboard/src/components/shared/Dashboard/ActionBtns.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,49 +8,48 @@ import {
   faFile,
 } from "@fortawesome/free-solid-svg-icons";
 
-type Btn = { label: string; icon: IconDefinition };
-const btnsData: Btn[] = [
+type Btn = { icon: IconDefinition };
+const btnsIcons: Btn[] = [
   {
-    label: "Add New Product",
     icon: faPlus,
   },
   {
-    label: "Create Sales Order",
     icon: faMoneyCheck,
   },
   {
-    label: "Create Purchase Order",
     icon: faMoneyCheck,
   },
   {
-    label: "View Low Stock",
     icon: faArrowTrendDown,
   },
   {
-    label: "Generate Report",
     icon: faFile,
   },
 ];
 
 export default function Action() {
+  const t = useTranslations("dashboard");
+
   return (
     <div>
       <h2 className="text-xl font-semibold mb-3">Quick Actions</h2>
 
       <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-2">
-        {btnsData.map((btn, i) => {
-          return <Btn key={i} data={btn} />;
+        {btnsIcons.map((btn, i) => {
+          return (
+            <Btn key={i} btn={btn} label={t(`actions.action-${i + 1}.title`)} />
+          );
         })}
       </div>
     </div>
   );
 }
 
-const Btn = ({ data }: { data: Btn }) => {
+const Btn = ({ btn, label }: { btn: Btn; label: string }) => {
   return (
     <button className="min-w-[250px] max-w-full flex justify-center items-center gap-2 bg-(--sidebar) text-white py-[15px] cursor-pointer border-[2px] border-transparent rounded-(--radius) hover:bg-transparent hover:text-(--sidebar) hover:border-(--sidebar)">
-      <FontAwesomeIcon icon={data.icon} />
-      <p className="pt-[5px]">{data.label}</p>
+      <FontAwesomeIcon icon={btn.icon} />
+      <p className="pt-[5px]">{label}</p>
     </button>
   );
 };
