f2ff1df41ced5863b7c4f3376addb20613146f94
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-02-05 17:50:58 +0100

    i18n: activity page (fr-en)

--- apps/dashboard/src/app/[locale]/(main)/account/activity/page.tsx
+++ apps/dashboard/src/app/[locale]/(main)/account/activity/page.tsx
@@ -1,4 +1,4 @@
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 import RecentActivity from "@/components/shared/account/activity";
 import { Activity } from "lucide-react";
 
@@ -9,10 +9,10 @@ export default function UserActivity() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="mb-8 text-center sm:text-left">
           <h1 className="flex items-center text-2xl sm:text-3xl font-bold text-foreground">
-            <Activity className="w-5 h-5 mr-2" />
+            <Activity className="w-5 h-5 mx-2" />
             {t("title")}
           </h1>
-          <p className="text-muted-foreground mt-2 text-sm sm:text-base">
+          <p className="w-fit text-muted-foreground mt-2 text-sm sm:text-base">
             {t("subtitle")}
           </p>
         </div>
