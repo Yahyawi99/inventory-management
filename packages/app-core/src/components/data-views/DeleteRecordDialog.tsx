9939d52f6a04ca7fe654d294e807e4d6d3ba1e8f
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-02-08 16:54:07 +0100

    i18n: personal settings page (en-fr-ar)

--- packages/app-core/src/components/data-views/DeleteRecordDialog.tsx
+++ packages/app-core/src/components/data-views/DeleteRecordDialog.tsx
@@ -103,7 +103,9 @@ export default function DeleteRecordDialog<T>({
             <Trash2 className="w-6 h-6 text-red-600" />
             {t("record_delete.title")}
           </DialogTitle>
-          <DialogDescription className="pt-2 w-fit">
+          <DialogDescription
+            className={`pt-2 w-fit ${locale === "ar" && "text-right"}`}
+          >
             {t("record_delete.desc")}
           </DialogDescription>
         </DialogHeader>
